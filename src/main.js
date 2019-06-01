cc38cc817c40407c1921dc77de188859fb624910
Haderson Bullón <hrbullon@gmail.com>
2019-06-01 19:23:22 -0400

    Initialized WebPage on VueJS

--- src/main.js
+++ src/main.js
@@ -1,8 +1,13 @@
 import Vue from 'vue'
 import App from './App.vue'
 
+//Styles
+import './assets/css/agency.css'
+import './assets/plugins/bootstrap/css/bootstrap.css'
+import './assets/plugins/fontawesome-free/css/all.css'
+
 Vue.config.productionTip = false
 
 new Vue({
-  render: h => h(App),
+    render: h => h(App),
 }).$mount('#app')
